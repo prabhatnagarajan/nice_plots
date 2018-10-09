267c275edbeba12fee91fd5ce33278d7807fcdc1
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2018-10-09 00:20:26 -0500

    adds linear interpolation to graphing

--- graph.py
+++ graph.py
@@ -1,16 +1,15 @@
 from pdb import set_trace
-import collections
 import numpy as np
-import matplotlib.colors as mc
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import rc
 # rc('text', usetex=True)
+from scipy.interpolate import interp1d
 
 #try bmh
 plt.style.use('bmh')
-
+from pdb import set_trace
 
 def sample_sd(data):
 	return np.std(data, ddof=1)
@@ -30,16 +29,28 @@ class Curve:
 	'''
 	def __init__(self, ys, xs=None, label=None, plot_ci=True):
 		if not xs:
-			xs = range(len(ys[0]))
+			xs = [range(len(ys[i])) for i in range(len(ys))]
 		self.label = label
 		self.xs = xs
 		self.ys = ys
 		self.plot_ci = plot_ci
 
+	def x_axis(self):
+		num_points = min([len(points) for points in self.ys])
+		min_x = max([min(xs) for xs in self.xs])
+		max_x = min([max(xs) for xs in self.xs])
+		increment = (min_x + max_x)/num_points
+		x_vals = [point * increment + min_x for point in range(num_points)]
+		if max_x not in x_vals:
+			x_vals.append(max_x)
+		return x_vals
+
+
 	def mean(self):
 		sample_size = len(self.ys)
-		mean = [float(sum([y[i] for y in self.ys]))/float(sample_size) for i in range(len(self.ys[0]))]
-		return mean
+		interp_ys = [interp1d(self.xs[i], self.ys[i]) for i in range(len(self.ys))]
+		x_vals = self.x_axis()
+		mean = [float(sum([f(i) for f in interp_ys]))/float(sample_size) for i in x_vals]
 	
 	def ci_error(self):
 		if len(self.ys) <= 1:
@@ -69,10 +80,10 @@ class Plot:
 		axes = plt.gca()
 		for curve in self.curves:
 			mean = np.array(curve.mean())
-			p = plt.plot(np.array(curve.xs), mean, label=curve.label)
+			p = plt.plot(np.array(curve.x_axis), mean, label=curve.label)
 			error = np.array(curve.ci_error())
 			if curve.plot_ci:
-				plt.fill_between(np.array(curve.xs), mean - error, mean + error,
+				plt.fill_between(np.array(curve.x_axis), mean - error, mean + error,
 				 alpha=0.5, edgecolor=p[0].get_color(),
 				facecolor=p[0].get_color())
 		# axes.set_ylim([0.0, 4.1])
